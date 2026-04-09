34bad9c6c669a27203d47cede2f99d4e6e668372
John <tve24el033@cet.ac.in>
2026-04-09 18:31:03 +0530

    c1

--- src/Solution.java
+++ src/Solution.java
@@ -5,17 +5,19 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        // TODO: Read a single line of text
-        
-        // TODO: Split the text into individual words
-        
-        // TODO: Create a HashMap to store the frequency of each word
-        
-        // TODO: Iterate through the words and update their frequencies in the map
-        
-        // TODO: Iterate through the map and print the unique words and their counts
-        // Format: "word: count"
-        
+        String text = scanner.nextLine();
+        String[] words = text.split("\\s+");
+        Map<String, Integer> frequencyMap = new HashMap<>();
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
+            }
+        }
+        String[] resultParts = new String[frequencyMap.size()];
+        int i = 0;
+        for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
+            resultParts[i++] = entry.getKey() + ": " + entry.getValue();
+        }
+        System.out.println(String.join(" ", resultParts));
     }
 }
